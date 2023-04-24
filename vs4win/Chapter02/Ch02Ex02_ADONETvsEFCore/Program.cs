d495d5ccc1ef707578f26de74ca7bc339421a6f7
Дмитрий Кулик <liamkukid@gmail.com>
2023-04-24 19:52:37 +0200

    Ch02Ex02_ADONETvsEFCore

---
+++ vs4win/Chapter02/Ch02Ex02_ADONETvsEFCore/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using Packt.Shared;
+using System.Data;
+using BenchmarkDotNet;
+
+var products = new List<Product>();
+using (var context = new NorthwindContext())
+{
+    products = context.Products.ToList();
+}
+
+SqlConnectionStringBuilder builder = new();
+builder.InitialCatalog = "Northwind";
+builder.MultipleActiveResultSets = true;
+builder.Encrypt = true;
+builder.TrustServerCertificate = true;
+builder.ConnectTimeout = 10;
+builder.DataSource = ".";
+builder.IntegratedSecurity = true;
+
+SqlConnection connection = new(builder.ConnectionString);
+connection.Open();
+
+SqlCommand cmd = connection.CreateCommand();
+cmd.CommandType = CommandType.Text;
+cmd.CommandText = "SELECT * FROM Products";
+SqlDataReader r = cmd.ExecuteReader();
+List<object[]> objects = new List<object[]>();
+while (r.Read())
+{
+    var row = new object[10];
+    r.GetValues(row);
+    objects.Add(row);
+}
+
+Console.ReadLine();
