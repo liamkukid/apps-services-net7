14c66ef3a1292f67bd21807db1bec94a810a0f7d
Дмитрий Кулик <liamkukid@gmail.com>
2023-04-25 19:14:02 +0200

    Benchmarks implemented

---
+++ vs4win/Chapter02/Ch02Ex02_ADONETvsEFCore/DBAccessBenchmarks.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Data.SqlClient;
+using Packt.Shared;
+using System.Data;
+
+namespace Ch02Ex02_ADONETvsEFCore;
+
+public class DBAccessBenchmarks
+{
+    [Benchmark]
+    public List<object[]> ADOTest()
+    {
+        SqlConnectionStringBuilder builder = new();
+        builder.InitialCatalog = "Northwind";
+        builder.MultipleActiveResultSets = true;
+        builder.Encrypt = true;
+        builder.TrustServerCertificate = true;
+        builder.ConnectTimeout = 10;
+        builder.DataSource = ".";
+        builder.IntegratedSecurity = true;
+
+        using SqlConnection connection = new(builder.ConnectionString);
+        connection.Open();
+
+        SqlCommand cmd = connection.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "SELECT * FROM Products";
+        SqlDataReader r = cmd.ExecuteReader();
+        List<object[]> objects = new List<object[]>();
+        while (r.Read())
+        {
+            var row = new object[10];
+            r.GetValues(row);
+            objects.Add(row);
+        }
+        return objects;
+    }
+
+    [Benchmark]
+    public List<Product> EFCoreTest()
+    {
+        var products = new List<Product>();
+        using (var context = new NorthwindContext())
+        {
+            products = context.Products.ToList();
+        }
+        return products;
+    }
+}
--- vs4win/Chapter02/Ch02Ex02_ADONETvsEFCore/Program.cs
+++ vs4win/Chapter02/Ch02Ex02_ADONETvsEFCore/Program.cs
@@ -1,36 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
-using Packt.Shared;
-using System.Data;
-using BenchmarkDotNet;
+﻿using BenchmarkDotNet.Running;
+using Ch02Ex02_ADONETvsEFCore;
 
-var products = new List<Product>();
-using (var context = new NorthwindContext())
-{
-    products = context.Products.ToList();
-}
 
-SqlConnectionStringBuilder builder = new();
-builder.InitialCatalog = "Northwind";
-builder.MultipleActiveResultSets = true;
-builder.Encrypt = true;
-builder.TrustServerCertificate = true;
-builder.ConnectTimeout = 10;
-builder.DataSource = ".";
-builder.IntegratedSecurity = true;
-
-SqlConnection connection = new(builder.ConnectionString);
-connection.Open();
-
-SqlCommand cmd = connection.CreateCommand();
-cmd.CommandType = CommandType.Text;
-cmd.CommandText = "SELECT * FROM Products";
-SqlDataReader r = cmd.ExecuteReader();
-List<object[]> objects = new List<object[]>();
-while (r.Read())
-{
-    var row = new object[10];
-    r.GetValues(row);
-    objects.Add(row);
-}
-
-Console.ReadLine();
+//var t = new DBAccessBenchmarks().ADOTest();
+BenchmarkRunner.Run<DBAccessBenchmarks>();
